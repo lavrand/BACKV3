cd51a8a8ef3a29e15e7cbe838e6ba474e3bdd11a
uraplayer <uraplayer@mail.ru>
2018-09-11 16:54:55 +0300

    test

---
+++ src/test/java/com.cvbank/controller/CVRestControllerTest.java
@@ -0,0 +1,105 @@
+package com.cvbank.controller;
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* CVRestController Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Sep 11, 2018</pre> 
+* @version 1.0 
+*/ 
+public class CVRestControllerTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: toList(Optional<T> option) 
+* 
+*/ 
+@Test
+public void testToList() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getAllCV(@CurrentProfile HttpServletRequest request) 
+* 
+*/ 
+@Test
+public void testGetAllCV() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: createNewCV(@RequestBody CV cv) 
+* 
+*/ 
+@Test
+public void testCreateNewCV() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getCV(@PathVariable Long id) 
+* 
+*/ 
+@Test
+public void testGetCV() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: updateCV(@RequestBody CV cv, @PathVariable Long id) 
+* 
+*/ 
+@Test
+public void testUpdateCV() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: deleteCV(@PathVariable Long id) 
+* 
+*/ 
+@Test
+public void testDeleteCV() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+/** 
+* 
+* Method: getProfile() 
+* 
+*/ 
+@Test
+public void testGetProfile() throws Exception { 
+//TODO: Test goes here... 
+/* 
+try { 
+   Method method = CVRestController.getClass().getMethod("getProfile"); 
+   method.setAccessible(true); 
+   method.invoke(<Object>, <Parameters>); 
+} catch(NoSuchMethodException e) { 
+} catch(IllegalAccessException e) { 
+} catch(InvocationTargetException e) { 
+} 
+*/ 
+} 
+
+} 

e4ad56a1e8630443bc2951d59b22d7ef7480126b
uraplayer <uraplayer@mail.ru>
2018-09-11 16:54:13 +0300

    test support

---
+++ src/test/java/com.cvbank/controller/RegistrationControllerTest.java
@@ -0,0 +1,16 @@
+package com.cvbank.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class RegistrationControllerTest {
+
+    @Test
+    public void authenticateUser() {
+    }
+
+    @Test
+    public void registerUser() {
+    }
+}
